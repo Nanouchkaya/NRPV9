79214e3125ff2713b93fa6192fc3c081f588bfd3
Nanouchkaya <brun.claudine@live.fr>
2020-04-16 19:29:27 +0200

    Popup script refactoring + add media queries for PA & NM-forumrow

--- popup/popup.js
+++ popup/popup.js
@@ -1,56 +1,78 @@
-// Display submenu on click event
-function toggleMenu(itemsLv1) {
-  itemsLv1.forEach(itemLv1 => {
-    itemLv1.addEventListener('click', (e) => {
-      var itemsLv2 = Array.from(e.target.children);
-      itemsLv2.forEach(itemLv2 => {
-        itemLv2.classList.toggle('visible');
+// Open popup on direct submenu content 
+async function openPopup(e) {
+  // 1 - get html from url and display it in popup
+  var url = e.target.dataset.url;
+  displayMessage(url);
+
+  // 2 - display popup
+  togglePopup();
+
+  var popupNavLinks = document.querySelectorAll('.popup-nav li');
+  popupNavLinks.forEach( popupNavLink => {
+    // 3 - open submenu in popup nav
+    if (url && url === popupNavLink.dataset.url) {
+      showListElements(popupNavLink);
+    }
+    // 4 - update content from popup navigation
+    else if(popupNavLink.dataset.url) {
+      popupNavLink.addEventListener('click', async (e) => {
+        var newUrl = e.target.dataset.url;
+        displayMessage(newUrl);
       });
-    })
+    }
+    // 5 - show or hide submenu
+    else if (popupNavLink.className === 'popup-list-lv1-item'
+    ||popupNavLink.className === 'popup-list-lv1-item visible' ) {
+      popupNavLink.addEventListener('click', toggleMenu);
+    }
+  });
+
+  // close popup when click occurs outside of it
+  document.querySelector('.jqmOverlay').addEventListener('click', () => {
+    popupNavLinks.forEach( popupNavLink => {
+      popupNavLink.classList.remove('visible');
+    });
+    togglePopup();
+  });
+}  
+
+// display all list elements in the list
+function showListElements(listItem) {
+  var siblings = [...listItem.parentElement.children];
+  siblings.forEach(li => {
+    li.classList.add('visible');
   });
 }
 
-
-
-// Display submenu on click event
-function toggleMenu(menu) {
-  if(typeof menu === 'object' || typeof menu === 'array'){
-    menu.forEach(itemLv1 => {
-      itemLv1.addEventListener('click', (e) => {
-        var itemsLv2 = Array.from(e.target.children);
-        // second level of menu
-        itemsLv2.forEach(itemLv2 => {
-          itemLv2.classList.toggle('visible');
-        });
-      })
-    });
-  } else {
-    menu.classList.toggle('visible');
-  }
+/**
+ * Display submenu on click event
+ * @param {string} itemLv1 : list element - from event listener
+ */
+function toggleMenu(itemLv1) {
+  var itemsLv2 = Array.from(itemLv1.target.children);
+  // second level of menu
+  itemsLv2.forEach(itemLv2 => {
+    itemLv2.classList.toggle('visible');
+  });
 }
 
 // Display content from url in popup content bloc  
-function displayMessage(links) {
-  links.forEach(link => {
-    link.addEventListener('click', (e) => {
-      console.log(e);
-      var url = e.target.dataset.url;
-      fetchMessage(url).catch(e => {
-        console.log('There has been a problem with your fetch operation: ' + e.message);
-        // General message to inform user - error
-        var errMsg = 'Oups! Il semblerait que les infos soient parties faire un tour... Tu peux les retrouver dans Vie du Forum. Bisous';
-        var errElt = document.createElement('p');
-        errElt.className = 'error';
-        errElt.textContent = errMsg;
-        document.querySelector('.popup-content').appendChild(errElt);
-      });
-    })
-  });
+async function displayMessage(url) {
+  var target = document.querySelector('.popup-content');
+  target.innerHTML = '';
+  try {
+    var text = await fetchMessage(url);
+    target.appendChild(text);
+  } catch {
+    var error = document.createElement('p');
+    error.className = 'error';
+    error.textContent = 'Oups! Il semblerait que les infos soient parties faire un tour... Tu peux les retrouver dans Vie du Forum. Bisous';
+    target.appendChild(error);
+  }
 }
 
 // Fetch post html content and add it to popup
 async function fetchMessage(url) {
-  console.log(url);
   let response = await fetch(url);
   let html = await response.text();
   
@@ -61,11 +83,16 @@ async function fetchMessage(url) {
   // Get the message content
   var idMsg = url.split('#')[1];
   var text = doc.querySelector('#msg-'+ idMsg +' .post-popup');
-  console.log(text + '#msg-'+ idMsg +' .post-popup');
-  var target = document.querySelector('.popup-content');
-  target.textContent = '';
-  target.appendChild(text);
+ 
+  return text;
 }
+
+function togglePopup() {
+  document.querySelector('.popup').classList.toggle('flex');
+  document.querySelector('.jqmOverlay').classList.toggle('visible');
+}
+
+
 
 // Display Popup
 /*
@@ -76,8 +103,3 @@ async function fetchMessage(url) {
 - écouteur sur la croix 
 - fermer popup et masquer overlay
 */
-
-function togglePopup(e) {
-  document.querySelector('.popup').classList.toggle('flex');
-  document.querySelector('.jqmOverlay').classList.toggle('visible');
-}
